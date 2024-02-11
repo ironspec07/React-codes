0dff99f5c6383098d1c3d1f012655c8e80d1b833
ironspec07 <ironspec07@gmail.com>
2024-02-11 20:40:03 +0530

    Scrimba React : Day 3

---
+++ Scrimba React/01_ReactSite/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return(
+        <nav>
+        <img src="./react-logo.png" alt="no image" width={80} />
+    </nav>
+    )
+};
+
+export default Header;
---
+++ Scrimba React/01_ReactSite/src/components/MainContent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MainContent() {
+    return (
+        <>
+            <h1>Fun Facts About React</h1>
+            <ul>
+                <li>Was first released in 2013.</li>
+                <li>Was originally created by Jordan Walke.</li>
+                <li>Has well over 100k stars on github.</li>
+                <li>Is maintained by facebook.</li>
+                <li>Powers thousand of enterprise apps including mobile apps.</li>
+                </ul>
+        </>
+    )
+};
+
+export default MainContent;
