ebfa466b7a4db9830eb3d64e7201df14ce68db75
ironspec07 <ironspec07@gmail.com>
2024-03-12 19:35:23 +0530

    Scrimba React : Day 11

---
+++ Scrimba React/03_Meme Generator/src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="../images/Troll.png" className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
