cd9de26724f7dbb16285eb274a8f107a24884033
ironspec07 <ironspec07@gmail.com>
2024-03-19 23:17:41 +0530

    Scrimba React : Day 13

--- Scrimba React/03_Meme Generator/src/components/meme.jsx
+++ Scrimba React/03_Meme Generator/src/components/meme.jsx
@@ -1,37 +1,37 @@
-import React from "react";
-import memesData from "../memesData";
+// import React from "react";
+// import memesData from "../memesData";
 
-export default function Meme() {
-    const [memeImage , setMemeImage] = React.useState("")
+// export default function Meme() {
+//     const [memeImage , setMemeImage] = React.useState("")
 
-    function getMemeImage() {
-        const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
-    }
+//     function getMemeImage() {
+//         const memesArray = memesData.data.memes
+//         const randomNumber = Math.floor(Math.random() * memesArray.length)
+//         setMemeImage(memesArray[randomNumber].url)
+//     }
 
-    return (
-        <main>
-            <div className="form">
-                <label htmlFor="top-text"> Top Text
-                    <input
-                        id="top-text"
-                        type="text" 
-                        className="form--input" 
-                        placeholder="Shut up"
-                    />
-                </label>
-                <label htmlFor="buttom-text"> Bottom Text
-                    <input
-                        id="buttom-text"
-                        type="text" 
-                        className="form--input" 
-                        placeholder="and take my money"
-                    />
-                </label>
-                <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
-                <img src={memeImage}  />
-            </div>
-        </main>
-    )
-}
+//     return (
+//         <main>
+//             <div className="form">
+//                 <label htmlFor="top-text"> Top Text
+//                     <input
+//                         id="top-text"
+//                         type="text" 
+//                         className="form--input" 
+//                         placeholder="Shut up"
+//                     />
+//                 </label>
+//                 <label htmlFor="buttom-text"> Bottom Text
+//                     <input
+//                         id="buttom-text"
+//                         type="text" 
+//                         className="form--input" 
+//                         placeholder="and take my money"
+//                     />
+//                 </label>
+//                 <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
+//                 <img src={memeImage}  />
+//             </div>
+//         </main>
+//     )
+// }
